b714e8cdf495eaa3bcad7b1d6b62126ff02845a7
Michalous <49964028+Michalous@users.noreply.github.com>
2023-01-06 18:04:34 +0000

    visual side updated

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
 document.addEventListener('DOMContentLoaded', function() {
     var submit = document.getElementById('submit')
     var resultOfDraw = document.getElementById('resultOfDraw')
-    var playerWins = 0
-    var cpuWins = 0
-    var draws = 0
+
+    var result1 = document.getElementById('result1')
+    var result2 = document.getElementById('result2')
+
+    var summary = document.getElementById('summary')
 
     submit.addEventListener('click', function() {
+        summary.classList.remove('finished')
         var pick = document.getElementById('pick').value
         var game = whoWins(pick)
         if (game == 'Draw') {
-            resultOfDraw.innerHTML = 'Draw'
+            resultOfDraw.innerHTML = 'DRAW'
         }
         else if (game == 'Player') {
-            resultOfDraw.innerHTML = "WIN!"
+            resultOfDraw.innerHTML = "User wins!"
         }
         else {
-            resultOfDraw.innerHTML = "LOST!"
+            resultOfDraw.innerHTML = "CPU wins!"
         }
         countGames(game)
+        
     })
 
     function CPUPlay() {
@@ -26,9 +30,13 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     function whoWins(pick) {
+        result1.classList.remove('active')
         var cpu = CPUPlay()
-        document.getElementById('cpu-played').innerHTML = cpu
-        console.log(pick, cpu)
+        result1.classList.add('active')
+        result2.classList.add('active')
+        setTimeout(removeClasses, 500)
+        
+        updateImages(pick, cpu)
         if (pick == cpu) {
             return 'Draw'
         }
@@ -44,32 +52,48 @@ document.addEventListener('DOMContentLoaded', function() {
         return 'Cpu'
     }
 
-    var writeWins = document.getElementById('wins')
-    var writeDraws = document.getElementById('draws')
-    var writeLoses = document.getElementById('loses')
-    
+    function updateImages(pick, cpu) {
+        document.getElementById('cpu-icon').src = `./assets/${cpu}.svg`
+        document.getElementById('user-icon').src = `./assets/${pick}.svg`
+    }
+
+    function removeClasses() {
+        result1.classList.remove('active')
+        result2.classList.remove('active')
+    }
+
+    var scores = {
+        "playerWins": 0,
+        "cpuWins": 0,
+        "draws": 0
+    }
+   
+    function updateStats() {
+    document.getElementById('wins').innerHTML = scores['playerWins']
+    document.getElementById('draws').innerHTML = scores['draws']
+    document.getElementById('loses').innerHTML = scores['cpuWins']
+    }
+
+
     function countGames(result) {
-        writeWins.innerHTML = ""
-        writeDraws.innerHTML = ""
-        writeLoses.innerHTML = ""
         if (result == 'Cpu') {
-            cpuWins++
+            scores['cpuWins'] += 1
         }
-        else if (result = 'Draw') {
-            draws++
+        else if (result == 'Player') {
+            scores['playerWins'] += 1
         }
         else {
-            playerWins++
+            scores['draws'] += 1
+        }
+        updateStats()
+
+        if (scores['cpuWins'] + scores['playerWins'] + scores['draws'] == 10) {
+            summary.classList.add('finished')
+            scores['cpuWins'] = 0
+            scores['playerWins'] = 0
+            scores['draws'] = 0
         }
 
-        if (playerWins + cpuWins + draws == 10) {
-            writeWins.innerHTML = 'Wins: ' + playerWins
-            writeDraws.innerHTML = 'Draws: ' + draws
-            writeLoses.innerHTML = 'Loses : ' + cpuWins
-            playerWins = 0
-            draws = 0
-            cpuWins = 0
-        }
     }
     
 })
